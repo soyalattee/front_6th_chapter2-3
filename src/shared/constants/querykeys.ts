1dc904ae2ad6b099f06e8dcee2a347d1ef7e9c0e
soyalattee <uyyu1734@gmail.com>
2025-08-14 02:52:23 +0900

    refactor: tanstack-query 에 사용할 쿼리키 파일 생성

---
+++ src/shared/constants/querykeys.ts
@@ -0,0 +1,7 @@
+export const QUERY_KEYS = {
+  POSTS: ["posts"],
+  USERS: ["users"],
+  COMMENTS: ["comments"],
+  TAGS: ["tags"],
+}
+//   TEAM_MEMBERS: (teamId: string) => ["team-members", teamId],
