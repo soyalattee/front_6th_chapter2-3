25f935f5447a2f96ad2104a930537c7633b0d7ec
soyalattee <uyyu1734@gmail.com>
2025-08-15 02:30:48 +0900

    refactor: any타입 제거하고 API함수 호출하도록 수정

--- src/entities/posts/ui/PostsTable.tsx
+++ src/entities/posts/ui/PostsTable.tsx
@@ -1,23 +1,22 @@
+import { PostWithAuthor } from "@/entities/postsManager/PostsManagerPage"
 import { Button, HighlightText, Table, useQueryParams } from "@/shared"
 import { Edit2, MessageSquare, ThumbsDown, ThumbsUp, Trash2 } from "lucide-react"
 
 interface PostsTableProps {
-  openPostDetail: (post: any) => void
-  openUserModal: (user: any) => void
-  posts: any[]
-  setPosts: React.Dispatch<React.SetStateAction<any[]>>
-  openEditDialog: (post: any) => void
+  openPostDetail: (post: PostWithAuthor) => void
+  openUserModal: (userId: number) => void
+  posts: PostWithAuthor[]
+  setPosts: React.Dispatch<React.SetStateAction<PostWithAuthor[]>>
+  openEditDialog: (post: PostWithAuthor) => void
 }
 
 export const PostsTable = ({ openPostDetail, openUserModal, openEditDialog, posts, setPosts }: PostsTableProps) => {
   const { searchQuery, updateURL, setSelectedTag, selectedTag } = useQueryParams()
 
   // 게시물 삭제
-  const deletePost = async (id) => {
+  const deletePost = async (id: number) => {
     try {
-      await fetch(`/api/posts/${id}`, {
-        method: "DELETE",
-      })
+      await deletePost(id)
       setPosts(posts.filter((post) => post.id !== id))
     } catch (error) {
       console.error("게시물 삭제 오류:", error)
@@ -66,17 +65,22 @@ export const PostsTable = ({ openPostDetail, openUserModal, openEditDialog, post
               </div>
             </Table.Cell>
             <Table.Cell>
-              <div className="flex items-center space-x-2 cursor-pointer" onClick={() => openUserModal(post.author)}>
-                <img src={post.author?.image} alt={post.author?.username} className="w-8 h-8 rounded-full" />
-                <span>{post.author?.username}</span>
-              </div>
+              {post.author && (
+                <div
+                  className="flex items-center space-x-2 cursor-pointer"
+                  onClick={() => post.author && openUserModal(post.author.id)}
+                >
+                  <img src={post.author.image} alt={post.author.username} className="w-8 h-8 rounded-full" />
+                  <span>{post.author.username}</span>
+                </div>
+              )}
             </Table.Cell>
             <Table.Cell>
               <div className="flex items-center gap-2">
                 <ThumbsUp className="w-4 h-4" />
-                <span>{post.reactions?.likes || 0}</span>
+                <span>{post.reactions.likes || 0}</span>
                 <ThumbsDown className="w-4 h-4" />
-                <span>{post.reactions?.dislikes || 0}</span>
+                <span>{post.reactions.dislikes || 0}</span>
               </div>
             </Table.Cell>
             <Table.Cell>
