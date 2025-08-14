7435d572e8f8c9f02c0f3618fcd5169db5fd61bc
soyalattee <uyyu1734@gmail.com>
2025-08-15 07:29:08 +0900

    fix: 새로 작성된 게시물은 reactions가 없을 수 있음

--- src/entities/posts/ui/PostsTable.tsx
+++ src/entities/posts/ui/PostsTable.tsx
@@ -67,9 +67,9 @@ export const PostsTable = ({ openPostDetail, openUserModal, openEditDialog, post
             <Table.Cell>
               <div className="flex items-center gap-2">
                 <ThumbsUp className="w-4 h-4" />
-                <span>{post.reactions.likes || 0}</span>
+                <span>{post.reactions?.likes || 0}</span>
                 <ThumbsDown className="w-4 h-4" />
-                <span>{post.reactions.dislikes || 0}</span>
+                <span>{post.reactions?.dislikes || 0}</span>
               </div>
             </Table.Cell>
             <Table.Cell>
